09ea9c45097107df85bdc839c3571dc0cdcd0ad6
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-11-27 16:16:21 -0400

    computed, readOnly, alias defined but never used
    modules/ember-bulma/components/bulma-message.js: line 3, col 8, 'computed' is defined but never used.
    modules/ember-bulma/components/bulma-message.js: line 3, col 20, 'readOnly' is defined but never used.
    modules/ember-bulma/components/bulma-message.js: line 3, col 30, 'alias' is defined but never used.

--- addon/components/bulma-message.js
+++ addon/components/bulma-message.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import layout from '../templates/components/bulma-message';
-import computed, { readOnly, alias } from 'ember-computed-decorators';
 import { _colorBindings, _responsiveHelpers } from '../constants';
 
 export default Ember.Component.extend({
